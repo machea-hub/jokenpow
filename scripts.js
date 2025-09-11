30ec05427cd27a9084e6a9ffa92c74117c3006bf
machea-hub <lucas.machea@hotmail.com>
2025-09-11 12:44:31 -0300

    responsividade e atualização lógica

--- scripts.js
+++ scripts.js
@@ -1,17 +1,19 @@
 const result = document.querySelector(".result");
 const humanScore = document.querySelector("#my-score");
 const machineScore = document.querySelector("#machine-score");
+const tiedScore = document.querySelector("#tied-score")
 
-humanScoreNumber = 0
-machineScoreNumber = 0
+tiedScoreNumber = 0;
+humanScoreNumber = 0;
+machineScoreNumber = 0;
 
 const playHuman = (humanChoice) => {
   playtTheGame(humanChoice, playMachine());
 };
 
 const playMachine = () => {
-  const choices = ["rock", "paper", "scissors"];
-  const randomNumber = Math.floor(Math.random() * 3);
+  const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+  const randomNumber = Math.floor(Math.random() * 6);
 
   return choices[randomNumber];
 };
@@ -20,11 +22,19 @@ const playtTheGame = (human, machine) => {
   console.log("Humano: " + human + ", Maquina: " + machine);
 
   if (human === machine) {
+    tiedScoreNumber++
+    tiedScore.innerHTML = tiedScoreNumber;
     result.innerHTML = "Deu empate";
   } else if (
     (human === "paper" && machine === "rock") ||
     (human === "rock" && machine === "scissors") ||
-    (human === "scissors" && machine === "paper")
+    (human === "scissors" && machine === "paper") ||
+    (human === "rock" && machine === "lizard") ||
+    (human === "lizard" && machine === "spock") ||
+    (human === "scissors" && machine === "lizard") ||
+    (human === "lizard" && machine === "paper") ||
+    (human === "paper" && machine === "spock") ||
+    (human === "spock" && machine === "rock")
   ) {
     humanScoreNumber++
     humanScore.innerHTML = humanScoreNumber
